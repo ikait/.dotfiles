839ab3133eee85ab4319a9ae7967bafa64689371
Taishi Ikai <t@ikai.tokyo>
2015-05-15 06:33:52 +0000

    fixed path

--- extend/pyenv.sh
+++ extend/pyenv.sh
@@ -1,9 +1,11 @@
 # パス追加
-if [ -e /usr/local/bin/rbenv -o -e /usr/bin/rbenv ]; then
-  eval "$(pyenv init -)"
 
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+if ! type pyenv > /dev/null 2>&1; then
+
+  eval "$(pyenv init -)"
 
   # pip インストール時に自動的に rehash 発動
   function pip(){
