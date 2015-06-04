7662744e71158cce785a9458ece5611c76569443
Taishi Ikai <t@ikai.tokyo>
2015-06-04 09:08:09 +0000

    modify pyenv.sh

--- extend/pyenv.sh
+++ extend/pyenv.sh
@@ -8,13 +8,13 @@ if type pyenv > /dev/null 2>&1; then
   eval "$(pyenv init -)"
 
   # pip インストール時に自動的に rehash 発動
-  function pip(){
-    $HOME/.pyenv/shims/pip $*
-    if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
-    then
-      pyenv rehash
-      rehash
-    fi
-  }
+  # function pip(){
+  #   $HOME/.pyenv/shims/pip $*
+  #   if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
+  #   then
+  #     pyenv rehash
+  #     rehash
+  #   fi
+  # }
 
 fi
