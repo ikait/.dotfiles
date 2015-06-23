a7347f589c98190dfff3b3e5dd2f50b14c4c47b5
Taishi Ikai <t@ikai.tokyo>
2015-06-23 16:59:13 +0000

    minor fix

--- extends/pyenv.sh
+++ extends/pyenv.sh
@@ -1,15 +1,17 @@
-if type /usr/local/bin/pyenv > /dev/null 2>&1; then
+if type $HOME/.pyenv/libexec/pyenv > /dev/null 2>&1; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
-
-  # pip インストール時に自動的に rehash 発動
-  # function pip(){
-  #   $HOME/.pyenv/shims/pip $*
-  #   if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
-  #   then
-  #     pyenv rehash
-  #     rehash
-  #   fi
-  # }
 fi
+
+eval "$(pyenv init -)"
+
+# pip インストール時に自動的に rehash 発動
+# function pip(){
+#   $HOME/.pyenv/shims/pip $*
+#   if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
+#   then
+#     pyenv rehash
+#     rehash
+#   fi
+# }
+
