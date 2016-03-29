d7619cc497f60bf3ef5984767161541244c91fcc
Taishi Ikai <t@ikai.tokyo>
2016-03-29 15:43:05 +0900

    Remove --all flag from tig

--- zshrc.sh
+++ zshrc.sh
@@ -137,7 +137,7 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
-alias tig='tig --all'
+alias tig='tig'
 
 
 #==========================================================================
