f58abe416eac8d19956130bdff5aa5bb08e18bf5
Taishi Ikai <t@ikai.tokyo>
2016-09-23 00:07:21 +0900

    Update nodebrew and pyenv1 extends
    Signed-off-by: Taishi Ikai <t@ikai.tokyo>

--- extends/nodebrew.sh
+++ extends/nodebrew.sh
@@ -1,1 +1,8 @@
-export PATH=$HOME/.nodebrew/current/bin:$PATH
+# if pyenv installed at home dir
+if type $HOME/.nodebrew/nodebrew > /dev/null 2>&1; then
+  export PATH=$HOME/.nodebrew/current/bin:$PATH
+fi
+# if pyenv installed by homebrew
+if type /usr/local/bin/nodebrew > /dev/null 2>&1; then
+  export PATH=/usr/local/var/nodebrew/current/bin:$PATH
+fi
