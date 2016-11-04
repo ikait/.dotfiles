2a4737e3de425ff87281e6d5f2fa05886ec16c92
Taishi Ikai <t@ikai.tokyo>
2016-11-04 10:39:00 +0900

    Modify to use sublime text 3 path

--- extends/sublime.sh
+++ extends/sublime.sh
@@ -1,4 +1,4 @@
-SRCPATH="/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+SRCPATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
 DSTPATH="/usr/local/bin/subl"
 
 if [ -e $SRCPATH ] && ! [ -e $DSTPATH ]; then
