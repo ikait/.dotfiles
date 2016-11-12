245386b2f471668274a1ae833cab5bd0700dc469
Taishi Ikai <t@ikai.tokyo>
2016-11-13 08:33:14 +0900

    Add some aliases

--- zshrc.sh
+++ zshrc.sh
@@ -133,11 +133,21 @@ alias ll='ls -alFh'
 alias la='ls -A'
 alias l='ls -CF'
 
+# grep の結果を常にカラーリング
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+# tig
 alias tig='tig'
+
+# IP
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
 
 
 #==========================================================================
