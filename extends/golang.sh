4d9ca313a43e48e2c9a42aa3d676925ba32a4e72
Taishi Ikai <t@ikai.tokyo>
2017-06-17 12:41:24 +0900

    Fix path setting

--- extends/golang.sh
+++ extends/golang.sh
@@ -1,5 +1,5 @@
 if type go > /dev/null 2>&1; then
-  export PATH=$PATH:/usr/local/opt/go/libexec/bin
+  export PATH=/usr/local/opt/go/libexec/bin:$PATH
   export GOPATH=$HOME/.go
-  export PATH=$PATH:$GOPATH/bin
+  export PATH=$GOPATH/bin:$PATH
 fi
