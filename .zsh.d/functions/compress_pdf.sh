4badeba7c9bf3d94517fb276bb29fc6b06dec8f0
Taishi Ikai <t@ikai.tokyo>
2024-03-08 11:49:10 +0900

    Add compresspdfall function to compress multiple PDF files

--- .zsh.d/functions/compress_pdf.sh
+++ .zsh.d/functions/compress_pdf.sh
@@ -6,3 +6,22 @@ compresspdf() {
         echo "Error: Install ghostscript."
     fi
 }
+
+compresspdfall() {
+    if which gs > /dev/null 2>&1; then
+        # 引数として指定されたすべてのファイルに対してループ処理
+        for input in "$@"
+        do
+        # 出力ファイル名の生成（元のファイル名に "_min" を追加）
+        output=${input%.*}_min.pdf
+
+        # Ghostscriptを使用してPDFの変換を実行
+        gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$output" "$input"
+
+        # 変換後のファイル名を表示
+        echo "$output"
+        done
+    else
+        echo "Error: Install ghostscript."
+    fi
+}
