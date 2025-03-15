ae6108f014e9869560ff72f774936a8babc78e66
Taishi Ikai <t@ikai.tokyo>
2025-03-15 23:52:21 +0900

    refactor(macos): simplify shell path configuration and update Homebrew installation commands

--- .zsh.d/extends/99_m1_homebrew.sh
+++ .zsh.d/extends/99_m1_homebrew.sh
@@ -1,4 +1,1 @@
-# if homebrew for m1 installed at home dir
-if type /opt/homebrew/bin/brew > /dev/null 2>&1; then
-  export PATH=/opt/homebrew/bin:$PATH
-fi
+export PATH="$(brew --prefix)/bin:$PATH"
