68fd4b7729405b8913757d1356c256aaad800543
Taishi Ikai <t@ikai.tokyo>
2025-05-30 09:28:12 +0900

    refactor(zsh): modify PATH export to append global npm bin directory

--- .zsh.d/extends/00_npm.sh
+++ .zsh.d/extends/00_npm.sh
@@ -2,6 +2,6 @@ if type npm > /dev/null 2>&1; then
   NPM_COMPLETION=~/.npm-completion
   npm completion > $NPM_COMPLETION
 
-  export PATH=/usr/local/share/npm/bin:$PATH
-  export PATH=./node_modules/.bin:$PATH
+  export PATH=$PATH:./node_modules/.bin
+  export PATH=$PATH:`npm prefix --location=global`/bin
 fi
